06c63775c5827a4e2b4330480630e4e554fa006f
Rudra <rudratheact@gmail.com>
2021-02-26 23:32:31 +0530

    XCTest

--- UserDemoSwiftUI/ContentView.swift
+++ UserDemoSwiftUI/ContentView.swift
@@ -7,15 +7,76 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+// MARK:- Model
+struct User: Identifiable {
+    let id: Int
+    let name: String
+    let location: String
+}
+
+// MARK:- API Call
+protocol DataService {
+    func getUsers(completion: @escaping ([User]) -> Void)
+}
+
+class AppDataService: DataService {
+    func getUsers(completion: @escaping ([User]) -> Void) {
+        DispatchQueue.main.async {
+            completion([
+                User(id: 1, name: "Alex", location: "USA"),
+                User(id: 2, name: "Sue", location: "China"),
+                User(id: 3, name: "Rishi", location: "India"),
+                User(id: 4, name: "Antony", location: "UK"),
+            ])
+        }
     }
+}
+
+// MARK:- View Model
+class ViewModel: ObservableObject {
+        @Published var users = [User]()
+        
+        let dataService: DataService
+        
+        init(dataService: DataService = AppDataService()) {
+            self.dataService = dataService
+        }
+        
+        func getUsers() {
+            dataService.getUsers { [weak self] users in
+                self?.users = users
+            }
+        }
+    }
+
+// MARK:- View
+struct ContentView: View {
+    @StateObject var viewModel: ViewModel
+       
+       init(viewModel: ViewModel = .init()) {
+           _viewModel = StateObject(wrappedValue: viewModel)
+       }
+       
+       var body: some View {
+           List(viewModel.users) { user in
+            HStack {
+                Text(user.name)
+                    .bold()
+                    .foregroundColor(.blue)
+                Spacer()
+                Text(user.location).italic()
+            }
+           }
+           .onAppear(perform: viewModel.getUsers)
+       }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-    }
+        let user = User(id: 0, name: "Dummy", location: "Denmark")
+            let viewModel = ViewModel()
+            viewModel.users = [user]
+            
+            return ContentView(viewModel: viewModel)
+        }
 }
--- UserDemoSwiftUITests/UserDemoSwiftUITests.swift
+++ UserDemoSwiftUITests/UserDemoSwiftUITests.swift
@@ -8,26 +8,38 @@
 import XCTest
 @testable import UserDemoSwiftUI
 
+class MockDataService: DataService {
+    func getUsers(completion: @escaping ([User]) -> Void) {
+        completion([User(id: 1, name: "John", location: "Sri Lanka")])
+    }
+}
+
 class UserDemoSwiftUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var sut: ViewModel!
+        
+        override func setUpWithError() throws {
+            sut = ViewModel(dataService: MockDataService())
         }
+
+        override func tearDownWithError() throws {
+            sut = nil
+        }
+        
+        func test_getUsers() throws {
+            XCTAssertTrue(sut.users.isEmpty)
+            
+            sut.getUsers()
+            
+            XCTAssertEqual(sut.users.count, 1)
+        }
+    
+    func test_getUserName() throws {
+        XCTAssertTrue(sut.users.isEmpty)
+        
+        sut.getUsers()
+        
+        XCTAssertEqual(sut.users.first?.name, "John")
     }
 
 }
