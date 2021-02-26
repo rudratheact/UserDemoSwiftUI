e0521ce60229e926ff9440284b82e1bf5f0a9d9a
Rudra <rudratheact@gmail.com>
2021-02-26 22:06:15 +0530

    Initial Commit

---
+++ UserDemoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserDemoSwiftUI
+//
+//  Created by Rudra on 26/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserDemoSwiftUI/UserDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDemoSwiftUIApp.swift
+//  UserDemoSwiftUI
+//
+//  Created by Rudra on 26/02/21.
+//
+
+import SwiftUI
+
+@main
+struct UserDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
